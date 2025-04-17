a74ee310bd5fddc0a2093169f37fdb6a9beeaf24
Gxbu1 <ggnyamu@usiu.ac.ke>
2025-04-17 15:22:46 +0300

    Add files via upload

---
+++ q1-IST 1025.py
@@ -0,0 +1,44 @@
+import math as md
+
+hourly_rate = int(500)
+hours_worked =int(40)
+tax_rate =int(50)
+
+#For this section  it outline the integers that are give for the program
+
+print("gross tax" + '=',hourly_rate * hours_worked )
+#This part is for the calculated gross tax by multyplying it by the hourly rate and its hours worked
+gross_salary = int(20000)
+
+if tax_rate is 0.15:    
+    tax_rate = float(0.15)
+
+tax=gross_salary * float(0.15)
+
+print(tax)
+#For this part it further calculates and deducts the tax from the gross salary based on the provided tax rate
+net_salary = gross_salary - tax
+
+print(net_salary)
+#For thi section it then calculates the net salary of the given integers after the tax
+
+print('''For the first line of codes above:
+They calculate and compute the gross salary by multiplying both the hoourly rate by its hours worked.
+It then deducts tax from the gross salary based on the provided tax rate.
+Then at its end it returns the net salary after the tax''')
+
+input(' Please enter your hourly rate and hours worked on ')
+
+calculate_salary = net_salary
+
+print(calculate_salary)
+#For this ssection it prompts the user to enter their hourly rate and hours worked
+
+
+
+
+
+
+
+
+
